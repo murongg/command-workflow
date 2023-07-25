74fc2043931de2e534ee61740ab96578fc6b008e
木荣 <372638156@qq.com>
2023-07-25 16:45:36 +0800

    chore: build

---
+++ rollup.config.js
@@ -0,0 +1,61 @@
+import { builtinModules } from 'node:module'
+import esbuild from 'rollup-plugin-esbuild'
+import dts from 'rollup-plugin-dts'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { defineConfig } from 'rollup'
+
+const input = 'src/index.ts'
+
+const external = [
+  ...builtinModules,
+]
+
+const plugins = [
+  nodeResolve({
+    preferBuiltins: true,
+  }),
+  commonjs(),
+  esbuild({
+    target: 'node14',
+  }),
+]
+
+export default () => defineConfig([
+  {
+    input,
+    output: {
+      dir: 'dist',
+      format: 'esm',
+      entryFileNames: '[name].mjs',
+    },
+    external,
+    plugins: [
+      ...plugins,
+    ],
+  },
+  {
+    input,
+    output: {
+      dir: 'dist',
+      format: 'cjs',
+      entryFileNames: '[name].cjs',
+    },
+    external,
+    plugins: [
+      ...plugins,
+    ],
+  },
+  {
+    input,
+    output: {
+      dir: 'dist',
+      entryFileNames: 'index.d.ts',
+      format: 'esm',
+    },
+    external,
+    plugins: [
+      dts({ respectExternal: true }),
+    ],
+  },
+])
