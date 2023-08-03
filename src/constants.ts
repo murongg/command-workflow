57b9542ca26f6fd0a7bdeb637561cf8d7ce34964
木荣 <372638156@qq.com>
2023-08-03 08:57:35 +0800

    refactor: load config use unconfig

--- src/constants.ts
+++ src/constants.ts
@@ -1,24 +1,6 @@
-export const DEFAULT_CONFIG_SUFFIXES = [
-  '.config.js',
-  '.config.mjs',
-  '.config.ts',
-  '.config.cjs',
-  '.config.mts',
-  '.config.cts',
-]
-
 export const DEFAULT_CONFIG_PREFIXES = [
   'cwf',
   'command-workflow',
   'command',
   'workflow',
 ]
-
-export const getDefaultConfigPrefixes = () => {
-  const files = []
-  for (const prefix of DEFAULT_CONFIG_PREFIXES) {
-    for (const suffix of DEFAULT_CONFIG_SUFFIXES)
-      files.push(`${prefix}${suffix}`)
-  }
-  return files
-}
--- test/constants.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from 'vitest'
-import { getDefaultConfigPrefixes } from '../src/constants'
-
-describe('constants', () => {
-  it('test getDefaultConfigPrefixes', () => {
-    const files = getDefaultConfigPrefixes()
-    expect(files).toMatchSnapshot()
-  })
-})
