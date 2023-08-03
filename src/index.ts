9089f1402d7d4929ff8428764fbde91bca737d6d
木荣 <372638156@qq.com>
2023-08-03 09:43:08 +0800

    feat: export all type

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { defineConfig } from './config'
 export { start } from './run'
+export type { StepTags, Step } from './types'
+export type { UserConfig, UserConfigFn, UserConfigMap, UserConfigExport } from './config'
